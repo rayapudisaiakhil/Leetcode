e16150c11726f118bde13848e1b39a151fd54276
saiakhilr <srayapudi@bluealpha.ai>
2025-12-24 20:54:10 -0500

    24Dec Neetcode 1

---
+++ containsduplicate.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        my_set = set()
+        for i in range(len(nums)):
+            if nums[i] in my_set:
+                return True
+            my_set.add(nums[i])
+        return False
+    
+arr = [1,2,3,3]
+obj1= Solution()
+print(obj1.hasDuplicate(arr))
